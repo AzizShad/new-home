97513fc599c8dd836e533a71e56874a50a20e427
Shad Aziz <shadaziz95@yahoo.com>
2021-01-06 18:43:32 -0800

    The beginning

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import elmo from './elmo.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={elmo}/>
+        <p>~ New Home ~</p>
+        <p>Game coming soon...</p>
       </header>
     </div>
   );
